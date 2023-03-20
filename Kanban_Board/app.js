fe3f7bfee8b1e0040c393dc1b3d438c4732508bb
AhmedMstafa <ahmed.mostafa.mail.1@gmail.com>
2023-03-20 21:41:14 +0200

    second

--- Kanban_Board/app.js
+++ Kanban_Board/app.js
@@ -1,107 +1,187 @@
-const columns = document.querySelectorAll(".column");
-// const listItems = document.querySelectorAll(".list-items")
-const addItem = document.querySelectorAll(".add-item");
-let drag = null;
-// let mainArray = [];
+// const columns = document.querySelectorAll(".column");
+// const addItem = document.querySelectorAll(".add-item");
+// let drag = null;
 
-// columns.forEach((column) => {
-//   let addBtn = column.querySelector(".add-item");
-//   addBtn.addEventListener("click", (e) => {
-//     let input = e.target.parentElement.querySelector(".item-input");
-//     console.log(input);
+// // Empty Array To Store The Tasks
+
+// let arrayOfTasks = [];
+
+// addItem.forEach((addItem) => {
+//   addItem.addEventListener("click", (e) => {
 //     let listItems = e.target.parentElement.querySelector(".list-items");
-//     let item = document.createElement("div");
-//     item.className = "item";
-//     item.setAttribute("draggable", true);
-//     item.innerHTML = `<input class="item-input" />
-//       <span class="item-control">
-//       <ion-icon name="create-outline"></ion-icon>
-//       <ion-icon name="trash-outline"></ion-icon>
-//       </span>
-//       `;
-//     listItems.prepend(item);
+//     let inputItem = e.target.parentElement.querySelectorAll(".item-input");
+//     let lastinputItem = inputItem[inputItem.length - 1];
+//     // console.log(lastinputItem);
+//     if (!lastinputItem || lastinputItem.textContent.trim() != "") {
+//       let item = document.createElement("div");
+//       item.className = "item";
+//       item.setAttribute("draggable", true);
+//       item.innerHTML = `<div class="item-input" contenteditable ></div>
+//     <span class="item-control">
+//     <ion-icon name="create-outline"></ion-icon>
+//     <ion-icon name="trash-outline"></ion-icon>
+//     </span>
+//     `;
+//       listItems.appendChild(item);
+//       // focus before add
+
+//       listItems.lastElementChild.querySelector(".item-input").focus();
+
+//       // add attribute before add
+
+//       listItems.lastElementChild
+//         .querySelector(".item-input")
+//         .addEventListener("blur", (e) => {
+//           e.target.removeAttribute("contenteditable");
+//           if (item.textContent.trim() != "") {
+//             addTaskToArray(item.textContent.trim(), addItem.parentElement.id);
+//             console.log(arrayOfTasks);
+//           }
+//         });
+//       listItems.lastElementChild
+//         .querySelector(".item-input")
+//         .addEventListener("keypress", (e) => {
+//           if (e.key === "Enter") {
+//             e.target.removeAttribute("contenteditable");
+//             if (item.textContent.trim() != "") {
+//               addTaskToArray(item.textContent.trim(), addItem.parentElement.id);
+//               console.log(arrayOfTasks);
+//             }
+//           }
+//         });
+
+//       // edit iteme before add
+
+//       listItems.lastElementChild
+//         .querySelector(".item-control :first-child")
+//         .addEventListener("click", (e) => {
+//           e.target.parentElement.previousElementSibling.setAttribute(
+//             "contenteditable",
+//             ""
+//           );
+//           e.target.parentElement.previousElementSibling.focus();
+//         });
+
+//       // remove item before add
+
+//       listItems.lastElementChild
+//         .querySelector(".item-control :last-child")
+//         .addEventListener("click", (e) => {
+//           e.target.parentElement.parentElement.remove();
+//           e.target.closest(".item").remove();
+//         });
+//     } else {
+//       alert("add item");
+//     }
+//     dragItem();
 //   });
-//   dragItem();
 // });
 
-addItem.forEach((addItem) => {
-  addItem.addEventListener("click", (e) => {
-    let listItems = e.target.parentElement.querySelector(".list-items");
-    let inputItem = e.target.parentElement.querySelectorAll(".item-input");
-    let lastinputItem = inputItem[inputItem.length - 1];
-    console.log(lastinputItem);
-    if (!lastinputItem || lastinputItem.innerHTML != "") {
-      let item = document.createElement("div");
-      item.className = "item";
-      item.setAttribute("draggable", true);
-      item.innerHTML = `<div class="item-input" contenteditable ></div>
-    <span class="item-control">
-    <ion-icon name="create-outline"></ion-icon>
-    <ion-icon name="trash-outline"></ion-icon>
-    </span>
-    `;
-      listItems.appendChild(item);
+// /// DragItems //////////////////////////////////////////
+// function dragItem() {
+//   let items = document.querySelectorAll(".item");
+//   items.forEach((item) => {
+//     item.addEventListener("dragstart", function () {
+//       drag = item;
+//     });
+//     item.addEventListener("dragend", function () {
+//       drag = null;
+//     });
 
-      // focus before add
+//     columns.forEach((column) => {
+//       column.addEventListener("dragover", function (e) {
+//         e.preventDefault();
+//       });
 
-      listItems.lastElementChild.querySelector(".item-input").focus();
+//       column.addEventListener("dragleave", function () {});
 
-      // add attribute before add
+//       column.addEventListener("drop", function () {
+//         this.querySelector(".list-items").appendChild(drag);
+//       });
+//     });
+//   });
+// }
 
-      listItems.lastElementChild
-        .querySelector(".item-input")
-        .addEventListener("blur", (e) => {
-          e.target.removeAttribute("contenteditable");
-        });
+// /////////////////////////////////////////////////////////
+// function addTaskToArray(taskText, id) {
+//   // Task Data
+//   const task = {
+//     id: Date.now(),
+//     title: taskText,
+//     ParentId: id,
+//   };
+//   arrayOfTasks.push(task);
 
-      // edit iteme before add
+//   // Add Tasks To Page
+//   // addElementsToPageFrom(arrayOfTasks) {}
+// }
+// console.log(arrayOfTasks);
 
-      listItems.lastElementChild
-        .querySelector(".item-control :first-child")
-        .addEventListener("click", (e) => {
-          e.target.parentElement.previousElementSibling.setAttribute(
-            "contenteditable",
-            ""
-          );
-          e.target.parentElement.previousElementSibling.focus();
-        });
+let arryOfTasks = [];
 
-      // remove item before add
+const addBtns = document.querySelectorAll(".add-item");
 
-      listItems.lastElementChild
-        .querySelector(".item-control :last-child")
-        .addEventListener("click", (e) => {
-          e.target.parentElement.parentElement.remove();
-          e.target.closest(".item").remove();
-        });
+addBtns.forEach((addBtn) => {
+  addBtn.addEventListener("click", (e) => {
+    const column = e.target.parentElement;
+    const itemInput = column.querySelectorAll(".item-input");
+    const lastItemInput = itemInput[itemInput.length - 1];
+    if (!lastItemInput || lastItemInput.textContent.trim() != "") {
+      addItem(e, column, lastItemInput);
     } else {
-      alert("add item");
     }
-    dragItem();
   });
 });
 
-/// DragItems //////////////////////////////////////////
-function dragItem() {
-  let items = document.querySelectorAll(".item");
-  items.forEach((item) => {
-    item.addEventListener("dragstart", function () {
-      drag = item;
-    });
-    item.addEventListener("dragend", function () {
-      drag = null;
-    });
+function addItem(e, column, itemInput) {
+  const listItems = column.querySelector(".list-items");
+  let item = document.createElement("div");
+  item.className = "item";
+  item.setAttribute("draggable", true);
+  item.innerHTML = `<div class="item-input" contenteditable ></div>
+  <span class="item-control">
+  <ion-icon name="create-outline"></ion-icon>
+  <ion-icon name="trash-outline"></ion-icon>
+  </span>
+  `;
+  listItems.appendChild(item);
 
-    columns.forEach((column) => {
-      column.addEventListener("dragover", function (e) {
-        e.preventDefault();
-      });
+  const lastItemInList =
+    listItems.lastElementChild.querySelector(".item-input");
+  // focus
+  lastItemInList.focus();
+  save(item, listItems);
+}
 
-      column.addEventListener("dragleave", function () {});
-
-      column.addEventListener("drop", function () {
-        this.querySelector(".list-items").appendChild(drag);
-      });
+function save(item, listItems) {
+  listItems.lastElementChild
+    .querySelector(".item-input")
+    .addEventListener("blur", (e) => {
+      e.target.removeAttribute("contenteditable");
+      if (item.textContent.trim() != "") {
+        addTaskToArray(listItems.parentElement, item.textContent.trim());
+      }
     });
-  });
+  listItems.lastElementChild
+    .querySelector(".item-input")
+    .addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        e.target.removeAttribute("contenteditable");
+        if (item.textContent.trim() != "") {
+          addTaskToArray(listItems.parentElement, item.textContent.trim());
+        }
+      }
+    });
+}
+
+function addTaskToArray(column, itemInput) {
+  const task = {
+    columnId: column.id,
+    contet: {
+      id: Date.now(),
+      tilte: itemInput,
+    },
+  };
+  arryOfTasks.push(task);
+  console.log(arryOfTasks);
 }
